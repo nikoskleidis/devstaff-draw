fddb1551950451814b529b9d973eceaec0e9dc5c
Zacharias Fragkiadakis <zacharias.fragkiadakis@fairlo.se>
2023-02-11 14:20:37 +0200

    Disable and remove app directory

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+const Home = () => {
+  return (
+    <main>
+      <h1>Home</h1>
+      <ul>
+        <li>
+          <Link href="/draw">Draw</Link>
+        </li>
+        <li>
+          <Link href="/participate">Participate</Link>
+        </li>
+      </ul>
+    </main>
+  );
+};
+
+export default Home;
