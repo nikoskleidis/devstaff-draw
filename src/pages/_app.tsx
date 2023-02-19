0cb438d8cc955e0f2a60550af04cd6b069909912
George Detorakis <giorgosdetor@gmail.com>
2023-02-19 21:08:37 +0200

    Add Mantine UI and font family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,33 @@ import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AppProps } from 'next/app';
+import { IBM_Plex_Sans } from '@next/font/google';
+import { MantineProvider } from '@mantine/core';
+
+const ibmPlexSans = IBM_Plex_Sans({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin']
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = React.useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        /** Put your mantine theme override here */
+        colorScheme: 'light'
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <div className={ibmPlexSans.className}>
+          <Component {...pageProps} />
+        </div>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </MantineProvider>
   );
 }
