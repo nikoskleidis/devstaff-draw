25bf075066222463e0b407fc8337249b2c111d50
Stavros Bastakis <staurosmpa@gmail.com>
2023-03-02 00:55:40 +0200

    Create Header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import Image from 'next/image';
 import styled from 'styled-components';
+import {Colors} from "@/src/constants";
+
 
 type Props = {
   children?: React.ReactNode;
@@ -21,19 +24,32 @@ const Main = styled.main`
 `;
 
 const Header = styled.div`
-  height: 20px;
-  background: gray;
+  padding: 10px 15px;
+  background: ${Colors.headerBg};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
+
+const Title = styled.span`
+  font-weight: bold;
+  font-size: 22px;
+  color: ${Colors.grey};
+`
 
 const Footer = styled.div`
   height: 20px;
   background: gray;
 `;
 
+
 const Layout = ({ children }: Props) => {
   return (
     <Wrapper>
-      <Header />
+        <Header>
+            <Image src='/devstaff-logo.webp' alt='Devstaff logo' width={90} height={50} />
+            <Title>DevStaff Draw</Title>
+        </Header>
 
       <Main>{children}</Main>
 
