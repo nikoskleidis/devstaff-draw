eebceb887de455322c7299e4158be3960d9c8e2b
Stavros Bastakis <staurosmpa@gmail.com>
2023-03-01 23:57:40 +0200

    Add styled-components

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   experimental: {
     appDir: false
-  }
+  },
+  compiler: {
+    // see https://nextjs.org/docs/advanced-features/compiler#styled-components for more info.
+    styledComponents: true
+  },
 };
 
 module.exports = nextConfig;
